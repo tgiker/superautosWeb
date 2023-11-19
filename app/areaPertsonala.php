7a2dadf44a0c3631c5f9e95ed4a97d86313e4fe3
tgiker <102799649+tgiker@users.noreply.github.com>
2023-11-19 17:00:09 +0100

    Add files via upload

--- app/areaPertsonala.php
+++ app/areaPertsonala.php
@@ -53,6 +53,9 @@
 	//anti-CSRF token gorde sesioan
 	$_SESSION['csrf_token'] = $csrfToken;
 
+	//X-Frame-Options konfigurazioa
+	header('X-Frame-Options: DENY');
+
 ?>
 
 <!DOCTYPE html>
--- app/hasiera.php
+++ app/hasiera.php
@@ -20,6 +20,9 @@
 	//anti-CSRF token gorde sesioan
 	$_SESSION['csrf_token'] = $csrfToken;
 
+	//X-Frame-Options konfigurazioa
+	header('X-Frame-Options: DENY');
+
 ?>
 <!DOCTYPE html>
 <html>
